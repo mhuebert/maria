236a05deb4552eeb5a4853bb430278872dfc31b1
Dave Liepmann <dave.liepmann@gmail.com>
2016-11-16 22:23:57 +0100

    Ideation for "lab" after first walkthrough
    INDEPENDENT EXPLORATION ÜBER ALLES

---
+++ src/maria/module_1_lab.clj
@@ -0,0 +1,16 @@
+(ns maria.module-1-lab)
+
+;; after completing module 1 as a walk-through they should be
+;; presented with a "problem" or creative task (ugh that word) that
+;; involves free creation using the tools they've been shown, plus
+;; coaching
+
+;; they should know:
+;; strings, true/false, numbers, functions, first/rest/last, conj, equality,
+;; TODO complete list
+
+;; TODO let them pick from at least 3-5 creative projects they could build/explore
+;; - introduce drawing fns and let them play with LEGOs
+;; - word puzzle (re-implement `subs`?)
+;; - tell an emoji story / make an emoji world on screen
+;; - mathy/CS stuff
