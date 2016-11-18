d9585673d696c4dd7845bc818ced35858bfb2690
Dave Liepmann <dave.liepmann@gmail.com>
2016-11-18 08:48:23 +0100

    Revise curriculum for faster introduction of fun

--- src/maria/module_1.clj
+++ src/maria/module_1.clj
@@ -13,53 +13,141 @@
 ;; Put your cursor to the right of "duck", after the quotation
 ;; mark. Press command-enter. Bam. You just evaluated a duck.
 
-;; (introduce evaluation, the tool, maybe terminology. minimal data
-;; types.)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; possibilities
-1
-2/3
-99999999.3 ;; if we do three numbers in a row, students will be bored to tears
-["duck" "hamster" "whale"] ;; god, vectors are boring if presented without context
-""
-"Aller Anfang ist schwer."
-"😀💥✨💖"
-"漢字" ;; kanji
-["h" "hi" "hip" "hipp" "hippo" "hippop" "hippopo" "hippopot" "hippopota" "hippopotam" "hippopotamu" "hippopotamus"]
-[]
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (introduce evaluation, the tool, maybe terminology. say nothing about data types.)
 
 ;; (introduce functions: asking the computer questions)
-(= "mouse" "rat")
 
-;; so that's `false`. what is `false`? 
+;; ("so, like, what are these things we're evaluating? Clojure will tell us. Clojure is helpful. Clojure is your friend. Clojure would never hurt you. Clojure just wants to be loved.")
 
-(= "duck" "duck") ;; and that's `true`
+;; we ask clojure questions like this:
 
-;; are these strings, like "duck"?
+(what-is "duck")
 
-(= true "true")
+;; wait, what did we just do?
 
-(= false "false")
+(what-is what-is)
 
-;; no...okay...
+;; we just asked Clojure a question by evaluating a function.
 
-;; (we walk them through true/false/string by introducing `what-is`)
+;; we gave it "duck" -- which is apparently a "string". what does Clojure give us back?
 
-(what-is what-is)  ;; maybe it goes here?
-(what-is true)
-(what-is false)
-(what-is "true")
-(what-is what-is) ;; ...or maybe it goes here?
-(what-is =)
-(what-is (what-is =)) ;; whoa
+(what-is (what-is "duck"))  ;; (have them eval both inner and outer form)
 
-;; less likely explorations that I'm keeping here as mental kibble:
+;; we gave a function a string, and it returned a string.
+
+;; --> a string is a "string" of characters (letters)
+
+(first "duck")
+
+;; weird.
+
+(what-is (first "duck"))
+
+;; so that's a character. hey, wait, that `first` thing. before evaluating anything, what do YOU think `first` is?
+
+;; ok, now ask Clojure:
+(what-is first)
+
+(comment
+  ;; XXX maybe an aside?
+  first ;; #function[clojure.core/first--4339]
+  )
+
+;; (anyway, back to our duck.)
+
+;; lots of things can be characters, and therefore strings. before evaluating, make a guess for what you think Clojure will tell you.
+
+"漢字"
+
+(what-is "漢字")
+
+(first "漢字")
+
+(what-is (first "漢字"))
+
 (what-is "😀💥✨💖")
-(what-is "love") ;; 🎵 baby don't hurt me 🎤 
-(what-is \d)
-;; //less likely explorations
+
+(what-is (first "😀💥✨💖")) ;; XXX eval'ing inner form may not return prettily; if so then nix this
+
+;; so, functions are how we ask the computer questions.
+
+;; we asked `first` of "😀💥✨💖" -- what if we want to ask about the 💥 or 💖 in "😀💥✨💖" ? how do you guess we could get to those characters in our string?
+
+;; to get 💥  from "😀💥✨💖" ?
+...
+
+;; to get 💖 from "😀💥✨💖" ?
+...
+
+;; XXX (these are very leading questions--which might be unavoidable at this stage. but generally one wants to avoid asking questions for which there is only one answer available for the student to guess)
+
+;; (maybe then show the general case with `nth`? that could also be stultifying)
+
+;; --------------
+;; XXX (possible exercise that might be too open-ended)
+;; what makes a string a string? try to create a string that the computer will recognize. (replace the ellipsis with your string)
+
+...
+
+;; and how would you check if your string is a string? (replace the ellipsis with how you'd ask)
+
+...
+
+;; so a string is "characters" that go between double quotes. let's push those boundaries. is there anyhing you could put between two double quotes that wouldn't make a string? try to break strings. brainstorm with your partner. try to confuse the computer.
+
+...
+...
+...
+
+;; XXX (that exercise might be too open-ended)
+;; --------------
+
+
+
+
+;; FIXME TODO 
+
+
+
+
+;; (transition to new example--string, number, vector, whatever. maybe something intentionally unwieldy.)
+
+;; let's pretend we're the red cross, and we need to get supplies to some refugee camps
+"38.4225° N, 27.1438° E"
+"37.0099° N, 37.7969° E"
+"36.6463° N, 37.0825° E"
+
+;; ok, but that's three separate strings. In Clojure, we do that like this:
+
+["38.4225° N, 27.1438° E"
+ "37.0099° N, 37.7969° E"
+ "36.6463° N, 37.0825° E"]
+
+;; what is that? ask the computer.
+
+...
+
+;; vectors are a(n indexed) collection of values. the same functions we used with strings work here:
+
+(first ["38.4225° N, 27.1438° E"
+        "37.0099° N, 37.7969° E"
+        "36.6463° N, 37.0825° E"])
+
+(second ["38.4225° N, 27.1438° E"
+         "37.0099° N, 37.7969° E"
+         "36.6463° N, 37.0825° E"])
+
+(last ["38.4225° N, 27.1438° E"
+       "37.0099° N, 37.7969° E"
+       "36.6463° N, 37.0825° E"])
+
+;; man, it's annoying to keep copying that whole vector around. couldn't we--
+
+(def camps ["38.4225° N, 27.1438° E"
+            "37.0099° N, 37.7969° E"
+            "36.6463° N, 37.0825° E"])
+
+;; 🎉🎉🎉
 
 
 ;;;; transition to inspecting vectors (maybe we introduced them as a
@@ -119,3 +207,4 @@ my-pet ;; still just the Schaferhund
 ;;;; (introduce fn)
 
 (fn [x] (inc x)) ;; this is so boring I'm in tears, please add emoji or SOMETHING
+
