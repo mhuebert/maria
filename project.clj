25bd51a89d81c7d4a69e0ce47c347eb7b31a09ad
Matthew Huebert <no-email@gmail.com>
2016-11-28 11:51:28 +0100

    New v. of cljs-live does not load cljs.core() automatically

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [cljsjs/codemirror "5.19.0-0"]
                  [cljsjs/marked "0.3.5-0"]
 
-                 [org.clojars.mhuebert/cljs-live "0.1.8-SNAPSHOT"]
+                 [org.clojars.mhuebert/cljs-live "0.1.9-SNAPSHOT"]
                  [org.clojars.mhuebert/re-view "0.2.3"]]
 
   :plugins [[lein-figwheel "0.5.8"]
