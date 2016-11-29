96b7eded918af886d7c2bba72f92d4eeab4315d4
Dave Liepmann <dave.liepmann@gmail.com>
2016-11-29 20:22:37 +0100

    Smooth out checkerboard example

--- src/maria/module_1.cljs
+++ src/maria/module_1.cljs
@@ -46,73 +46,79 @@
 
 (circle 20 20)
 
-;; If we write code that Clojure can't understand, it will tell us it
-;; can't run it. If it can, Clojure will try to tell you how to fix
-;; your code.
+;; That's an error message. If we write code that Clojure can't
+;; understand, it will tell us it can't run it. If it can, Clojure
+;; will try to tell you how to fix your code.
 
 ;; What do you think this next expression will draw?
 
-(colorize "blue" (circle 20))
+(colorize "blue" (rectangle 200 50))
 
 ;; We colorized our circle by passing it to another function. Notice
-;; how the `circle` function is nested inside the `colorize` function?
+;; how the `rectangle` function is nested inside the `colorize`
+;; function?
 
 ;; Let's draw a bunch of shapes side-by-side using the `line-up` function:
 
 (line-up (circle 25)
-         (rectangle 50 50)
          (circle 25)
-         (rectangle 50 50)
          (circle 25)
-         (rectangle 50 50)
          (circle 25)
-         (rectangle 50 50)
          (circle 25)
-         (rectangle 50 50))
+         (circle 25)
+         (circle 25)
+         (circle 25)
+         (circle 25))
 
 ;; Man, that was a lot of typing. And so repetitive! When I see myself
 ;; repeating the same code over and over, I get suspicious.
 
 ;; Luckily, we can code ourselves a shorter solution. We don't have to
-;; type `(rectangle 50 50)` and `(circle 25)` over and over. We can
-;; type it once and Clojure will remember it for us:
+;; type `(circle 25)` over and over. We can type it once and Clojure
+;; will remember it for us:
 
-(let [c (circle 25) ;; XXX just one pair at first?
-      r (rectangle 50 50)] 
-  ;; Now we can type just `c` and `r`:
+(let [c (circle 25)]
+  ;; Now we can type just `c`:
+  (line-up c c c c c c c c c))
+
+;; We used `let` to define a name `c` for our expression. The `let`
+;; function's first parameter is a pair of square brackets (`[]`) with
+;; pairs of names and values inside.
+
+;; We can have multiple pairs if we want:
+
+(let [c (circle 25)
+      r (rectangle 50 50)]
   (line-up c r c r c r c r c r))
 
-;; We used `let` to define names (`c` and `r`) for our
-;; expressions. The `let` function's first parameter is a pair of
-;; square brackets (`[]`) with pairs of names and values inside.
-
-;; Can you make all the circles one color, and the squares another?
+;; Can you change that expression to make all the circles one color
+;; and the squares another?
 
 ...
 
-;; OK. Let's say we wanted to draw a checkerboard: a big square made
-;; up of lots of little squares, alternating colors. Let's start with
-;; our smallest unit: a single square. We'll build our checkerboard
-;; code step by step, so that it gradually grows into the final shape
-;; we want.
+;; Nice. Now let's say we wanted to draw a checkerboard: a big square
+;; made up of lots of little squares, alternating colors. Let's start
+;; with our smallest unit: a single square. We'll build our
+;; checkerboard code step by step, so that it gradually grows into the
+;; final shape we want.
 
-(square 5) ;; ERROR
+(square 50) ;; ERROR
 
-;; Oops. Clojure doesn't know squares--only rectangles.
+;; Oops. Clojure knows rectangles, but apparently not squares.
 
-(rectangle 25 25)
+(rectangle 50 50)
 
 ;; We add a dash of color...
 
-(colorize "red" (rectangle 25 25))
+(colorize "red" (rectangle 50 50))
 
-(colorize "black" (rectangle 25 25))
+(colorize "black" (rectangle 50 50))
 
 ;; We can put those together with `let` to make a 2x2 checker. That
 ;; will be our building block.
 
-(let [r (colorize "red" (rectangle 25 25))
-      b (colorize "black" (rectangle 25 25))]
+(let [r (colorize "red" (rectangle 50 50))
+      b (colorize "black" (rectangle 50 50))]
   (stack (line-up r b)
          (line-up b r)))
 
@@ -120,28 +126,30 @@
 ;; that with the `repeat` function, which just calls a function as
 ;; many times as it's told.
 
-(let [r (colorize "red" (rectangle 25 25)) ;; same as above
-      b (colorize "black" (rectangle 25 25)) ;; same as above
+(let [r (colorize "red" (rectangle 50 50)) ;; same as above
+      b (colorize "black" (rectangle 50 50)) ;; same as above
       checker (stack (line-up r b)
                      (line-up b r))]
   (apply line-up (repeat 4 checker)))
+;; XXX XXX XXX apply is waaaaay too advanced!
 
 ;; ...and we `repeat` the rows to make a complete board. (Again, we
 ;; repeat our `r`, `b`, and `checker` definitions in our `let`.)
 
-(let [r (colorize "red" (rectangle 25 25))
-      b (colorize "black" (rectangle 25 25))
+(let [r (colorize "red" (rectangle 50 50))
+      b (colorize "black" (rectangle 50 50))
       checker (stack (line-up r b)
                      (line-up b r)) 
       row (apply line-up (repeat 4 checker))]
-  (stack (repeat 4 row)))
+  (apply stack (repeat 4 row)))
+;; XXX XXX XXX apply is waaaaay too advanced!
 
 ;; Cool!
 
 ;; Now, what if we wanted to play with that checkerboard? To draw five
 ;; of them across the screen, or to show it to friends later? Our
 ;; `(let)` can only do one thing at a time, and is getting too long
-;; anyway. We're still repeating ourselves too much.
+;; anyway. We're still repeating ourselves too much!
 
 ;; What we need is to create a name for our checkerboard and use it
 ;; anywhere we want--not just inside a `let`. For that, we use `def`:
