e48674c599ef00eb6886a690b5e8a6e20e16a2d7
Dave Liepmann <dave.liepmann@gmail.com>
2017-06-10 09:42:21 +0200

    Iterate quick intro per Jack's comments

--- doc/quick_intro.cljs
+++ doc/quick_intro.cljs
@@ -6,24 +6,41 @@
 
 "puppy"
 
-;; Put your cursor on the same line as "puppy" and press `control-enter` (`command-enter` on Mac). BOOM. You just evaluated a puppy. This expression evaluates to itself: "puppy". Clojure (the programming language we're using) printed the evaluation of our "puppy" expression in the output panel on the right.
+;; Put your cursor at the end of the line with "puppy" and press `control-enter` (`command-enter` on Mac). BOOM. You just evaluated a puppy. This expression evaluates to itself: "puppy". Clojure (the programming language we're using) printed the evaluation of our "puppy" expression in the output panel on the right.
 
 ;; That's how you'll explore: type and evaluate expressions on the left; and read the result of your evaluations on the right.
 
 
 ;;;; Asking the Computer Questions
 
-;; You can ask the computer questions by calling functions, which will return expressions. To call a function, put an open parenthesis, then the function name, then parameters you give the function, and then a close parenthesis. It looks like this:
+;; You can think of expressions like "puppy" as nouns. They describe a thing and they evaluate to themselves. Expressions that are verbs return something other than themselves, and we call those functions. Functions are how we ask the computer questions or to do something for us.
+
+;; We write these function expressions inside parentheses that hold the function name (the "verb") followed by all the parameters (the "nouns" you give the function). It looks like this:
 
 (circle 50)
 
-;; The function is `circle` and you give it the parameter `50`. Evaluating the `circle` function asks the computer to draw a circle with a 50-pixel radius.
+;; The "verb" (function name) is `circle`, and we give it the "noun" `50`. Everything stays inside the parentheses, so there's no confusion about which verbs go with which nouns. When you evaluate this expression you're asking the computer to draw a circle with a 50-pixel radius.
 
 ;; What if you didn't know that `circle` needed to be given a radius? How would you find out the parameters to a function? Why, you can ask your friend the computer!
 
+(what-is circle)
+
+;; TODO transitions in this whole section
+
+(what-is what-is)
+
+;; If you're not sure what a certain expression is, you can ask!
+
+(what-is 50)
+
+;; TODO (explain _)
+
+;; FIXME transition
 (doc circle)
 
 ;; The `doc` function is short for "documentation". It tells you what a function does and what parameters it needs. You gave `doc` the parameter `circle`, so it gives you documentation for the `circle` function. The computer wants to help you.
+
+;; TODO (once we have nice results) try and fail (doc "puppy")
 
 ;; Try giving circle the wrong number of parameters, just to see what happens:
 
@@ -79,21 +96,6 @@
 ;; If you have some time, take a minute and play around a little. Make your own composite shapes, evaluating inner expressions to make sure you know how they fit into the expression containing them.
 
 
-;;;; More questions to ask
-
-;; You already saw how `doc` is a way to ask Clojure what it knows about a function. You can also ask the computer what kind of thing something is. What was that value you evaluated up above?
-
-(what-is "puppy")
-
-;; We'll explore all sorts of Clojure expressions and values together, and so Maria wants you to have `what-is` by your side in case you're ever curious or confused. So...what is `what-is`?
-
-(what-is what-is)
-
-;; Please don't be shy about using `doc` and `what-is`. Ask the computer what it knows. It's like a dog that never gets tired of playing fetch: it will keep answering you as long as you have questions to ask.
-
-;; TODO add parameter hints to their toolbelt here -- if their cursor is inside parens enclosing a function, show the arglist for that function
-
-
 ;;;; Powers of fun
 
 ;; What if you want to draw a whole bunch of shapes? Typing "rectangle" over and over again is a chore. You don't have to live like that. Our friend the computer wants to help us, and it is REALLY GOOD at repetitive chores.
@@ -105,6 +107,10 @@
 (what-is [2 4 8 16 32 64 128])
 
 ;; OK. So square brackets mean a vector. This vector has numbers in it.
+
+;; TODO maybe introduce (vector) here?
+;; TODO maybe (map vector [1 2 3])
+;; TODO maybe then (map vector [1 2 3] [4 5 6])
 
 ;; What's `map`?
 
@@ -122,14 +128,36 @@
 
 ;; This returns the result of evaluating `circle` with radius 2, then radius 4, then radius 8, and so on for each number in our vector.
 
-;; One neat thing about `map` is that you can give it more than one collection. To make that work, you need to `map` a function that uses more than one parameter. We'll use `colorize`.
 
-;; `colorize` takes a color and a shape. That means that if you want to `map` with the `colorize` function, you can give it two collections: first a collection of colors, and then a collection of shapes. Then `map` will execute `colorize` using the first element of each collection, then the second element of each collection, then the third, and so on. So:
+;; XXX NEW
+;; Let's try something similar with `rectangle`.
+
+(map rectangle [10 20 5 50 100])
+
+;; Hmm...all squares is fine, but what if we want to draw a city skyline, or a bar chart? We can just give `map` a vector of widths AND a vector of heights, and it will stitch them together for us:
+
+(map rectangle
+     [5 5 5 5 5 5 5 5 5 5 5 5]
+     [10 20 5 50 100 70 76 33 20 90])
+
+;; Does it annoy you that you had to type out `5` a bunch of times? It annoys me. It's so tedious. Did I even get the number of `5`s right? Thankfully, our friend the computer LOVES doing things over and over, and we can just ask it to `repeat` as many `5`s as we need:
+
+(map rectangle
+     (repeat 5)
+     [10 20 5 50 100 70 76 33 20 90])
+
+;; How does `repeat` do that?
+
+(doc repeat)
+
+;; Wow. Infinity.😲😯😵
+
+;; We can do the same thing with colors: `colorize` takes a color and a shape. That means that if you want to `map` with the `colorize` function, you can give it a vector of colors and then a vector of shapes. Then `map` will execute `colorize` using the first element of each vector, then the second element of each vector, then the third, and so on. So:
 (map colorize
      ["red" "blue" "yellow"]
      [(rectangle 20 20) (rectangle 50 50) (rectangle 100 100)])
 
-;; The first element of the first collection and the first element of the second collection get used to call the function. So that expression with `map` is like evaluating each of these individual expressions:
+;; The first element of the first vector and the first element of the second vector get used to call the function. So that expression with `map` is like evaluating each of these individual expressions:
 
 (colorize "red" (rectangle 20 20))
 (colorize "blue" (rectangle 50 50))
@@ -138,12 +166,14 @@
 
 ;;;; The power of names
 
-;; Often when programming you need to name something you've created. For instance, you can let the name "palette" be a vector of colors:
+;; Often when programming you need to name something you've created. For instance, you can let the name "palette" be a vector of colors:;; FIXME
 
 (let [palette ["red" "orange" "yellow" "green" "blue" "purple"]]
-  (rand-nth palette))
+  (map colorize
+       palette
+       (repeat (circle 50))))
 
-;; You can read this as saying, "Let 'palette' be the name for this vector, ['red' 'orange' blah blah blah] while we evaluate the next expression." The next expression says, "Get a random element from 'palette'." Evaluate it a few times--you'll see a different random color from your palette every time!
+;; And there's our color palette. Lovely. You can read this as saying, "Let 'palette' be the name for this vector, ['red' 'orange' blah blah blah] while we evaluate the next expression."
 
 ;; Names can be helpful, but too many names scattered across your code gets hard to keep track of. That's why we use `let` to create names we only need for right now. Watch--try to use `palette` outside the `let`:
 
@@ -154,7 +184,7 @@ palette
 ;; If you need a name that you'll use over and over, you need to define it with `def`:
 (def palette ["red" "orange" "yellow" "green" "blue" "purple"])
 
-;; Now we can use `palette` anywhere.
+;; Now you can use `palette` anywhere. You can blindly grab a color from our palette with `rand-nth`, which is a random-picker function:
 (colorize (rand-nth palette) (circle 50))
 
 ;; Let's make a more complex shape with our new color palette.
@@ -164,7 +194,15 @@ palette
             ;; FIXME requires creating triangle function
             [(circle 50) (triangle 100 100 100) (rectangle 100 100)]))
 
-;; It's kind of annoying that we must repeat ourselves for those random colors. And we don't have to! If you need to do something over and over, you can create a function with `fn`:
+;; It's kind of annoying that we must repeat ourselves for those random colors. And we don't have to! Just like `repeat` will give you as many "nouns" you need, `repeatedly` will give you as many "verby" function calls you need.
+
+(doc repeatedly)
+
+;; So to use `repeatedly`, we need a function. But `(rand-nth palette)` isn't a function, it's a function *call*--it evaluates to some random color name, not the function itself:
+
+(what-is (rand-nth palette))
+
+;; What we need to do is create a function, which we can do `fn`:
 (what-is (fn [] (rand-nth palette)))
 
 ;; All the functions you've used so far have had names, but this one doesn't, because we're just using it once.
@@ -176,15 +214,15 @@ palette
             ;; FIXME requires creating triangle function
             [(circle 50) (triangle 100 100 100) (rectangle 100 100)]))
 
-;; `repeatedly` will go on forever if you ask it to, but it knows you only need to call that function as many times as there are shapes. (Clojure is crafty like that.)
+;; `repeatedly` will go on forever just like `repeat` if you let it, but it knows you only need to call that function as many times as there are shapes. (Clojure is crafty like that.)
 
-;; You can get an infinite list of shapes, too, as long as you tell it how many you want:
+;; We can use an infinite number of circles alongside our infinite random color choices, as long as we say how many we want to `take` from the infinite bucket:
 (apply line-up
        (map colorize
-            (take 3 (repeatedly (fn [] (rand-nth palette))))
+            (take 10 (repeatedly (fn [] (rand-nth palette))))
             (repeat (circle 50))))
 
-;; You know, we keep using that anonymous function. That's a good sign that maybe we should name it. Just like with `palette`, we use `def`:
+;; You know, we keep using that anonymous function. That's a good sign that maybe we should name it. Just like we named `palette`, we use `def` to name our function:
 (def rand-color (fn [] (rand-nth palette)))
 
 ;; Now `rand-color` stands shoulder-to-shoulder with `circle` and `map`:
