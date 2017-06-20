a84c29b2d3f295e539e0cb0db4677425a56c6f98
Jack Rusher <jack@rusher.com>
2017-06-20 11:46:00 +0200

    doc -> use what-is for non-functions

--- src/maria/repl_specials.cljs
+++ src/maria/repl_specials.cljs
@@ -4,14 +4,6 @@
             [maria.views.repl-specials :as special-views]
             [maria.ns-utils :as ns-utils]))
 
-(defspecial doc
-  "Show documentation for given symbol"
-  [c-state c-env name]
-  {:value (special-views/doc (merge {:expanded?   true
-                                     :standalone? true}
-                                    (or (ns-utils/resolve-var c-state c-env name)
-                                        (some-> (get e/repl-specials name) (meta)))))})
-
 (defspecial dir
   "Display public vars in namespace"
   [c-state c-env ns]
@@ -26,6 +18,21 @@
                                                                    (and (symbol? thing)
                                                                         (contains? e/repl-specials thing)) :maria.kinds/function
                                                                    :else thing))))))
+
+(defspecial doc
+  "Show documentation for given symbol"
+  [c-state c-env name]
+  (if (fn? name)
+    {:value (special-views/doc (merge {:expanded?   true
+                                       :standalone? true}
+                                      (or (ns-utils/resolve-var c-state c-env name)
+                                          (some-> (get e/repl-specials name) (meta)))))}
+    ;; XXX ugly copy/paste because macros are weird in cljs
+    (let [macro (when (symbol? name) (:macro (ns-utils/resolve-var c-state c-env name)))]
+      (e/eval-str c-state c-env (str `(maria.messages/what-is ~(cond macro :maria.kinds/macro
+                                                                     (and (symbol? name)
+                                                                          (contains? e/repl-specials name)) :maria.kinds/function
+                                                                     :else name)))))))
 
 (defspecial inject
   "Inject vars into a namespace, preserving all metadata (inc. name)"
