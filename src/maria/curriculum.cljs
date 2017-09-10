90c24dd4cad0968db48431346bd26635f0cd4301
Matthew Huebert <no-email@gmail.com>
2017-09-11 00:26:06 +0200

    Fix curriculum link

--- src/maria/curriculum.cljs
+++ src/maria/curriculum.cljs
@@ -1,8 +1,8 @@
 (ns maria.curriculum
   (:require [clojure.set :as set]))
 
-(def modules-by-path {"Intro"   "6121050c023ad640688e1d0220c0f50d"
-                      "Gallery" "c41203d2f973c838ee0ee4aed32d0679"})
+(def modules-by-path {"intro"   "6121050c023ad640688e1d0220c0f50d"
+                      "gallery" "c41203d2f973c838ee0ee4aed32d0679"})
 (def modules-by-id (set/map-invert modules-by-path))
 
 (def modules-owner {:username  "modules"
