fa74facd025835deab6f292b27912a0b3ddb3a92
Matthew Huebert <mhuebert@gmail.com>
2017-09-14 15:41:24 +0200

    Add editor quickstart

--- maria-editor/src/maria/curriculum.cljs
+++ maria-editor/src/maria/curriculum.cljs
@@ -1,8 +1,9 @@
 (ns maria.curriculum
   (:require [clojure.set :as set]))
 
-(def modules-by-path {"intro"   "6121050c023ad640688e1d0220c0f50d"
-                      "gallery" "c41203d2f973c838ee0ee4aed32d0679"})
+(def modules-by-path {"intro"             "6121050c023ad640688e1d0220c0f50d"
+                      "gallery"           "c41203d2f973c838ee0ee4aed32d0679"
+                      "Editor Quickstart" "7fd1790db0ed178995bab207a2250f0e"})
 (def modules-by-id (set/map-invert modules-by-path))
 
 (def modules-owner {:username  "modules"
