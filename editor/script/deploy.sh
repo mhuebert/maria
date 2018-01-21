6dd93b8dfb06173f6348675162f550019217ad53
Matthew Huebert <mhuebert@gmail.com>
2018-01-22 00:28:08 +0100

    fix(build): try passing token explicitly

--- editor/script/deploy.sh
+++ editor/script/deploy.sh
@@ -2,10 +2,10 @@
 case "$CIRCLE_BRANCH" in
 "release")
   echo "Deploying to www.maria.cloud:";
-  npx firebase deploy -P prod;
+  npx firebase deploy -P prod --token "$FIREBASE_TOKEN";
   ;;
 "master")
   echo "Deploying to dev.maria.cloud";
-  npx firebase deploy -P dev;
+  npx firebase deploy -P dev --token "$FIREBASE_TOKEN";
   ;;
 esac
