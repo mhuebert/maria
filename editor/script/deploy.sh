7a7d34bbe1c21e4525f49eda6cf931e24203eb50
Matthew Huebert <mhuebert@gmail.com>
2018-01-22 01:10:09 +0100

    build(deploy-vars): use correct firebase app ids

--- editor/script/deploy.sh
+++ editor/script/deploy.sh
@@ -2,10 +2,10 @@
 case "$CIRCLE_BRANCH" in
 "release")
   echo "Deploying to www.maria.cloud:";
-  npx firebase deploy -P prod --token "$FIREBASE_TOKEN";
+  npx firebase deploy -P "$FIREBASE_PROD" --only hosting;
   ;;
 "master")
   echo "Deploying to dev.maria.cloud";
-  npx firebase deploy -P dev --token "$FIREBASE_TOKEN";
+  npx firebase deploy -P "$FIREBASE_DEV" --only hosting;
   ;;
 esac
