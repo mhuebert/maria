eb6954588386e5c2f1984a7beb6331759de7802e
Matthew Huebert <mhuebert@gmail.com>
2018-01-22 01:34:02 +0100

    build(script): use clj script to release

---
+++ editor/src/maria/build.clj
@@ -0,0 +1,10 @@
+(ns maria.build
+  (:require [shadow.cljs.devtools.api :as api]))
+
+;; to run from command line: npx shadow-cljs clj-run maria.build/release
+
+(defn release
+  []
+  (api/release :live)
+  (api/release :trusted)
+  (api/release :bootstrap))
