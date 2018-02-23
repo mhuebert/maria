57f6c99c9d4ab204bb7cd2408ec35344cbfa9431
Matthew Huebert <mhuebert@gmail.com>
2018-02-23 23:04:14 +0100

    fix(keyboard-shortcut-hints)

--- editor/src/maria/views/floating/tooltip.cljs
+++ editor/src/maria/views/floating/tooltip.cljs
@@ -9,35 +9,37 @@
             [cljs.tools.reader.edn :as edn]))
 
 (defview Tooltip
-  {:view/did-mount    (fn [{:keys [view/state]}]
-                        (let [the-events #js ["mouseover" "mouseout" "mousedown" "mouseenter" "mouseleave"]
-                              body (.-body js/document)
-                              callback (fn [e]
-                                         (let [target (.-target e)
-                                               body-event (#{"mouseenter" "mouseleave" "mousedown"} (.-type e))]
-                                           (if body-event
+  {:view/did-mount
+   (fn [{:keys [view/state]}]
+     (let [the-events #js ["mouseover" "mouseout" "mousedown" "mouseenter" "mouseleave"]
+           body (.-body js/document)
+           callback (fn [e]
+                      (let [target (.-target e)
+                            body-event (#{"mouseenter" "mouseleave" "mousedown"} (.-type e))]
+                        (if body-event
 
-                                             (swap! state dissoc :tooltip)
-                                             (when (data/has target "tooltip")
-                                               (swap! state assoc :tooltip
-                                                      ;; mouseenter and mouseleave only trigger on currentTarget (body)
-                                                      (when (or (= "mouseover" (.-type e))
-                                                                (some-> (.-relatedTarget e)
-                                                                        (r/closest #(and (not= % js/document)
-                                                                                         (data/has % "tooltip")))))
-                                                        {:float/pos    (util/rect->abs-pos (.getBoundingClientRect target)
-                                                                                           [:center :bottom])
-                                                         :float/offset [0 5]
-                                                         :content      (edn/read-string (data/get target "tooltip"))}))))))]
-                          (events/listen body the-events callback)
-                          (v/swap-silently! state assoc :unlisten #(events/unlisten body the-events callback))))
+                          (swap! state dissoc :tooltip)
+                          (when (data/has target "tooltip")
+                            (swap! state assoc :tooltip
+                                   ;; mouseenter and mouseleave only trigger on currentTarget (body)
+                                   (when (or (= "mouseover" (.-type e))
+                                             (some-> (.-relatedTarget e)
+                                                     (r/closest #(and (not= % js/document)
+                                                                      (data/has % "tooltip")))))
+                                     {:float/pos (util/rect->abs-pos (.getBoundingClientRect target)
+                                                                     [:center :bottom])
+                                      :float/offset [0 5]
+                                      :content (edn/read-string (data/get target "tooltip"))}))))))]
+       (events/listen body the-events callback)
+       (v/swap-silently! state assoc :unlisten #(events/unlisten body the-events callback))))
    :view/will-unmount #((:unlisten @(:view/state %)))}
   [{:keys [view/state]}]
   (when-let [{:keys [content float/pos float/offset]} (:tooltip @state)]
-    (ui/FloatingContainer {:float/pos    pos
+    (ui/FloatingContainer {:float/pos pos
                            :float/offset offset
-                           :element      [:.absolute.tc
-                                          {:style {:width 150
-                                                   :left  -75}}
-                                          [:.pa1.br1.bg-black.white.f7.sans-serif.dib content]]})))
+                           :element [:.absolute.tc
+                                     {:style {:width 150
+                                              :left -75}}
+                                     [:.pa1.br2.bg-black.white.f7.sans-serif.dib
+                                      content]]})))
 
--- editor/src/maria/views/top_bar.cljs
+++ editor/src/maria/views/top_bar.cljs
@@ -26,30 +26,32 @@
    (some-> icon (toolbar-icon))
    (when (and icon text) util/space)
    (cond->>
-     [:.dib.truncate text]
-     icon (conj [:.dn.dib-ns]))])
+    [:.dib.truncate text]
+    icon (conj [:.dn.dib-ns]))])
 
 (defn command-button
   [context command-name {:keys [icon else-icon tooltip text]}]
   (let [tooltip (when-not (false? tooltip) (or tooltip (registry/spaced-name (name command-name))))
-        {:keys [exec? parsed-bindings]} (exec/get-command context command-name)
-        key-string (some-> (ffirst parsed-bindings) (registry/keyset-string))]
+        {:keys [exec? bindings]} (exec/get-command context command-name)
+        key-string (some-> (first bindings)
+                           (registry/binding-string->vec)
+                           (registry/keyset-string))]
     (if exec?
       (toolbar-button [#(do (util/stop! %)
                             (exec/exec-command-name command-name)) icon text (if key-string
-                                                                               [:.flex.items-center tooltip
-                                                                                [:.o-80.ml1 key-string]]
+                                                                               [:.items-center tooltip
+                                                                                [:.o-80.nowrap key-string]]
                                                                                tooltip)])
       (when else-icon
         (toolbar-button [nil else-icon nil tooltip])))))
 
 
 (defview fixed-top
-  {:view/did-mount    (fn [{:keys [view/state]}]
-                        (->> (events/listen js/window "scroll"
-                                            (gf/throttle (fn [e]
-                                                           (swap! state assoc :scrolled? (not= 0 (.-scrollY js/window)))) 300))
-                             (v/swap-silently! state assoc :listener-key)))
+  {:view/did-mount (fn [{:keys [view/state]}]
+                     (->> (events/listen js/window "scroll"
+                                         (gf/throttle (fn [e]
+                                                        (swap! state assoc :scrolled? (not= 0 (.-scrollY js/window)))) 300))
+                          (v/swap-silently! state assoc :listener-key)))
    :view/will-unmount #(events/unlistenByKey (:listener-key @(:view/state %)))}
   [{:keys [when-scrolled view/state]} child]
   [:.fixed.top-0.right-0.left-0.z-5.transition-all
@@ -58,36 +60,36 @@
                (assoc-in [:style :left] (d/get :ui/globals :sidebar-width)))) child])
 
 (defview doc-toolbar
-  {:view/did-mount          (fn [this]
-                              (.updateWindowTitle this)
-                              (some->> (:id this) (doc/locals-push! :local/recents)))
-   :view/will-unmount       (fn [this]
-                              (when (= (:current-doc @exec/context) this)
-                                (exec/set-context! {:current-doc nil})))
-   :view/will-mount         (fn [this] (exec/set-context! {:current-doc this}))
-   :view/will-receive-props (fn [{filename                                 :filename
-                                  props                                    :view/props
+  {:view/did-mount (fn [this]
+                     (.updateWindowTitle this)
+                     (some->> (:id this) (doc/locals-push! :local/recents)))
+   :view/will-unmount (fn [this]
+                        (when (= (:current-doc @exec/context) this)
+                          (exec/set-context! {:current-doc nil})))
+   :view/will-mount (fn [this] (exec/set-context! {:current-doc this}))
+   :view/will-receive-props (fn [{filename :filename
+                                  props :view/props
                                   {prev-filename :filename :as prev-props} :view/prev-props
-                                  :as                                      this}]
+                                  :as this}]
                               (when-not (= filename prev-filename)
                                 (.updateWindowTitle this))
                               (when (not= props prev-props)
                                 (some->> (:id this)
                                          (doc/locals-push! :local/recents))))
-   :get-filename            (fn [{:keys [filename] :as this}]
-                              (get-in this [:project :local :files filename :filename] filename))
-   :update-window-title     (fn [{:keys [view/state] :as this}]
-                              (let [filename (.getFilename this)]
+   :get-filename (fn [{:keys [filename] :as this}]
+                   (get-in this [:project :local :files filename :filename] filename))
+   :update-window-title (fn [{:keys [view/state] :as this}]
+                          (let [filename (.getFilename this)]
 
-                                #_(when (= filename "Untitled.cljs")
-                                    (js/setTimeout #(some-> (:title-input @state)
-                                                            :view/state
-                                                            (deref)
-                                                            :input-element
-                                                            (.select)) 50))
-                                (frame/send frame/trusted-frame [:window/set-title (util/some-str filename)])))}
+                            #_(when (= filename "Untitled.cljs")
+                                (js/setTimeout #(some-> (:title-input @state)
+                                                        :view/state
+                                                        (deref)
+                                                        :input-element
+                                                        (.select)) 50))
+                            (frame/send frame/trusted-frame [:window/set-title (util/some-str filename)])))}
   [{{:keys [persisted local]} :project
-    :keys                     [filename id view/state left-content] :as this}]
+    :keys [filename id view/state left-content] :as this}]
   (let [signed-in? (d/get :auth-public :signed-in?)
         {parent-url :local-url parent-username :username} (or (:owner persisted)
                                                               (:owner this))
@@ -96,78 +98,79 @@
         {persisted-content :content} (get-in persisted [:files filename])
         update-filename #(d/transact! [[:db/update-attr id :local (fn [local]
                                                                     (assoc-in local [:files filename] {:filename %
-                                                                                                       :content  (or local-content persisted-content)}))]])
+                                                                                                       :content (or local-content persisted-content)}))]])
         command-context (exec/get-context)
         {:keys [persistence/provider remote-url]} persisted
         persistence-mode (doc/persistence-mode this)
         sidebar? (d/get :ui/globals :sidebar?)]
     [:div
      (fixed-top
-       {:when-scrolled {:style {:background-color "#e7e7e7"
-                                :border-bottom    "2px solid #e2e2e2"}}}
-       [:.flex.sans-serif.items-stretch.f7.flex-none.overflow-hidden.pl2
+      {:when-scrolled {:style {:background-color "#e7e7e7"
+                               :border-bottom "2px solid #e2e2e2"}}}
+      [:.flex.sans-serif.items-stretch.f7.flex-none.overflow-hidden.pl2
 
-        (when-not sidebar?
-          (list
-            (toolbar-button [{:on-click #(d/transact! [[:db/update-attr :ui/globals :sidebar? (comp not boolean)]])}
-                             icons/Docs
-                             nil
-                             (if sidebar? "Hide Sidebar" "Docs")])))
+       (when-not sidebar?
+         (list
+          (toolbar-button [{:on-click #(d/transact! [[:db/update-attr :ui/globals :sidebar? (comp not boolean)]])}
+                           icons/Docs
+                           nil
+                           (if sidebar? "Hide Sidebar" "Docs")])))
 
-        (command-button command-context :doc/new {:text    "New"
-                                                  :tooltip "New Doc"})
+       (command-button command-context :doc/new {:text "New"
+                                                 :tooltip false})
 
-        (some->>
-          (or left-content
-              (when filename
-                (list
-                  [:.ph2.flex.items-center
-                   (when (and parent-username parent-url)
-                     [:a.hover-underline.gray.no-underline.dn.dib-ns {:href parent-url} parent-username])
-                   [:.ph1.gray.dn.dib-ns "/"]
-                   (text/autosize-text {:auto-focus  true
-                                        :class       "mr2 half-b sans-serif"
-                                        :ref         #(when % (swap! state assoc :title-input %))
-                                        :value       (doc/strip-clj-ext current-filename)
-                                        :on-key-down #(cond (and (= 13 (.-which %))
-                                                                 (not (or (.-metaKey %)
-                                                                          (.-ctrlKey %))))
-                                                            (doc/persist! this)
-                                                            (= 40 (.-which %))
-                                                            (exec/exec-command-name :navigate/focus-start)
-                                                            :else nil)
-                                        :placeholder "Enter a title..."
-                                        :on-change   #(update-filename (doc/add-clj-ext (.-value (.-target %))))})]
-                  )))
-          (conj [:.flex.items-stretch.bg-darken-lightly]))
+       (some->>
+        (or left-content
+            (when filename
+              (list
+               [:.ph2.flex.items-center
+                (when (and parent-username parent-url)
+                  [:a.hover-underline.gray.no-underline.dn.dib-ns {:href parent-url} parent-username])
+                [:.ph1.gray.dn.dib-ns "/"]
+                (text/autosize-text {:auto-focus true
+                                     :class "mr2 half-b sans-serif"
+                                     :ref #(when % (swap! state assoc :title-input %))
+                                     :value (doc/strip-clj-ext current-filename)
+                                     :on-key-down #(cond (and (= 13 (.-which %))
+                                                              (not (or (.-metaKey %)
+                                                                       (.-ctrlKey %))))
+                                                         (doc/persist! this)
+                                                         (= 40 (.-which %))
+                                                         (exec/exec-command-name :navigate/focus-start)
+                                                         :else nil)
+                                     :placeholder "Enter a title..."
+                                     :on-change #(update-filename (doc/add-clj-ext (.-value (.-target %))))})]
+               )))
+        (conj [:.flex.items-stretch.bg-darken-lightly]))
 
-        (case persistence-mode
-          (:save :create)
-          (command-button command-context :doc/save {:text    "Save"
-                                                     :tooltip (str (if persisted "Saves Gist"
-                                                                                 "Publishes a new gist"))})
-          :saved
-          (toolbar-button [nil nil [:span.o-60 "Saved"] nil])
-          nil)
-        (command-button command-context :doc/revert-to-saved-version {:text "Revert"})
+       (case persistence-mode
+         (:save :create)
+         (command-button command-context :doc/save {:text "Save"
+                                                    :tooltip (str (if persisted false
+                                                                                "Publish as new Gist"))})
+         :saved
+         (toolbar-button [nil nil [:span.o-60 "Saved"] nil])
+         nil)
+       (command-button command-context :doc/revert-to-saved-version {:text "Revert"})
 
-        (command-button command-context :doc/duplicate {:text "Duplicate"})
+       (command-button command-context :doc/duplicate {:text "Duplicate"
+                                                       :tooltip false})
 
-        (when (= provider :gist)
-          (toolbar-button [{:href   remote-url
-                            :target "_blank"} icons/OpenInNew nil "View on GitHub"]))
+       (when (= provider :gist)
+         (toolbar-button [{:href remote-url
+                           :target "_blank"} icons/OpenInNew nil "View on GitHub"]))
 
-        [:.flex-auto]
+       [:.flex-auto]
 
-        (command-button command-context :commands/command-search {:text "Commands..."})
+       (command-button command-context :commands/command-search {:text "Commands..."})
 
-        (toolbar-button [{:href      "https://www.github.com/mhuebert/maria/issues"
-                          :tab-index -1
-                          :target    "_blank"} nil "Bug Report"])
-        (if (d/get :auth-public :signed-in?)
-          (toolbar-button [#(doc/send [:auth/sign-out]) icons/SignOut nil "Sign out"])
-          (toolbar-button [#(frame/send frame/trusted-frame [:auth/sign-in]) nil "Sign in with GitHub"]))
-        [:.ph1]])
+       (toolbar-button [{:href "https://www.github.com/mhuebert/maria/issues"
+                         :tab-index -1
+                         :target "_blank"} nil "Bug Report"])
+       (if (d/get :auth-public :signed-in?)
+         (toolbar-button [#(doc/send [:auth/sign-out]) icons/SignOut nil "Sign out"])
+         (toolbar-button [#(frame/send frame/trusted-frame [:auth/sign-in]) nil "Sign in with GitHub"]))
+       [:.ph1]])
      [:.h2]
      ]))
 
