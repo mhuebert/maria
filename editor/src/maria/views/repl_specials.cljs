ff3def7270b7e2ce7128939c1dfcafee15391900
Ryan Zebian <zebian93@gmail.com>
2018-02-26 22:27:18 -0800

    add clojuredoc url encoding

--- editor/src/maria/views/repl_specials.cljs
+++ editor/src/maria/views/repl_specials.cljs
@@ -10,7 +10,7 @@
 (defn docs-link [namespace name]
   (when (re-find #"^(cljs|clojure)\.core(\$macros)?$" namespace)
     [:.mv2
-     [:a.f7.black {:href   (str "https://clojuredocs.org/clojure.core/" (string/replace-first name #"\?$" "_q"))
+     [:a.f7.black {:href   (str "https://clojuredocs.org/clojure.core/" (ns-utils/cd-encode name))
                    :target "_blank"
                    :rel    "noopener noreferrer"} "clojuredocs ↗"]]))
 
