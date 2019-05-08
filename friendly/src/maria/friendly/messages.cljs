846616787af5aa0b6f37c061ac7ed5316c193a97
Dave Liepmann <dave.liepmann@gmail.com>
2019-05-08 14:22:47 +0200

    Whitespace

--- friendly/src/maria/friendly/messages.cljs
+++ friendly/src/maria/friendly/messages.cljs
@@ -131,23 +131,20 @@
   [prev-row current-element other-seq pred]
   (reduce
    (fn [row [diagonal above other-element]]
-     (let [update-val
-	       (if (pred other-element current-element)
-	         ;; if the elements are deemed equivalent according to the predicate
-	         ;; pred, then no change has taken place to the string, so we are
-	         ;; going to set it the same value as diagonal (which is the previous edit-distance)
-	         diagonal
+     (conj row (if (pred other-element current-element)
+	             ;; if the elements are deemed equivalent according to the predicate
+	             ;; pred, then no change has taken place to the string, so we are
+	             ;; going to set it the same value as diagonal (which is the previous edit-distance)
+	             diagonal
 
-	         ;; in the case where the elements are not considered equivalent, then we are going
-	         ;; to figure out if its a substitution (then there is a change of 1 from the previous
-	         ;; edit distance) thus the value is diagonal + 1 or if its a deletion, then the value
-	         ;; is present in the columns, but not in the rows, the edit distance is the edit-distance
-	         ;; of last of row + 1 (since we will be using vectors, peek is more efficient)
-	         ;; or it could be a case of insertion, then the value is above+1, and we chose
-	         ;; the minimum of the three
-	         (inc (min diagonal above (peek row)))
-	         )]
-	   (conj row update-val)))
+	             ;; in the case where the elements are not considered equivalent, then we are going
+	             ;; to figure out if its a substitution (then there is a change of 1 from the previous
+	             ;; edit distance) thus the value is diagonal + 1 or if its a deletion, then the value
+	             ;; is present in the columns, but not in the rows, the edit distance is the edit-distance
+	             ;; of last of row + 1 (since we will be using vectors, peek is more efficient)
+	             ;; or it could be a case of insertion, then the value is above+1, and we chose
+	             ;; the minimum of the three
+	             (inc (min diagonal above (peek row))))))
    ;; we need to initialize the reduce function with the value of a row, since we are
    ;; constructing this row from the previous one, the row is a vector of 1 element which
    ;; consists of 1 + the first element in the previous row (edit distance between the prefix so far
@@ -163,21 +160,20 @@
   "Levenshtein Distance - http://en.wikipedia.org/wiki/Levenshtein_distance
   In information theory and computer science, the Levenshtein distance is a metric for measuring the amount of difference between two sequences. This is a functional implementation of the levenshtein edit
   distance with as little mutability as possible.
-  Still maintains the O(n*m) guarantee.
-  "
-  [a b & {p :predicate  :or {p =}}]
-  (peek
-   (reduce
-    ;; we use a simple reduction to convert the previous row into the next-row  using the
-    ;; compute-next-row which takes a current element, the previous-row computed so far
-    ;; and the predicate to compare for equality.
-    (fn [prev-row current-element]
-	  (compute-next-row prev-row current-element b p))
+  Still maintains the O(n*m) guarantee."
+  [a b & {p :predicate :or {p =}}]
+  (peek (reduce
+         ;; we use a simple reduction to convert the previous row into
+         ;; the next-row using the compute-next-row which takes a
+         ;; current element, the previous-row computed so far and the
+         ;; predicate to compare for equality.
+         (fn [prev-row current-element]
+	       (compute-next-row prev-row current-element b p))
 
-    ;; we need to initialize the prev-row with the edit distance between the various prefixes of
-    ;; b and the empty string.
-    (map #(identity %2) (cons nil b) (range)) 
-    a)))
+         ;; we need to initialize the prev-row with the edit distance
+         ;; between the various prefixes of b and the empty string.
+         (map #(identity %2) (cons nil b) (range))
+         a)))
 
 (defn similar-to-vars ;; TODO improve this name
   "Finds `n` (default 4) Maria-relevant Clojure vars (as Strings) named similarly to the given String `s`."
@@ -198,14 +194,14 @@
 (comment
   (similar-to-vars "define" 20) ;; it would be nice to prioritize `def` higher
   (similar-to-vars "defun") 
-  (similar-to-vars "def") ;; XXX not known!
-  (similar-to-vars "cirlce")
+  (similar-to-vars "def")
+  (similar-to-vars "cirlce")  
   (similar-to-vars "whatis")
   (similar-to-vars "js-source1") ;; <-- TODO
 
   ;; It's hard to find the right match with short names:
-  (similar-to-vars "tryy")
-  (similar-to-vars "tyr") ;; we'd like to see `try` but it's tied with all the rest
+  (similar-to-vars "tryy") ;; works fine
+  (similar-to-vars "tyr") ;; we'd like to see `try` but it's tied with all the other candidates
   (similar-to-vars "tyr" 10)
 
   )
