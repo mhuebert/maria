fe384afdafad9e8e1350846f04bf9ad0959ac6d3
Matthew Huebert <mhuebert@gmail.com>
2019-11-15 14:09:59 +0100

    use module-hash-names

--- editor/src/maria/static.clj
+++ editor/src/maria/static.clj
@@ -1,18 +1,28 @@
 (ns maria.static
-  (:require [cljs-static.page :as page]))
+  (:require [cljs-static.page :as page]
+            [cljs-static.shadow :as shadow]
+            [hiccup2.core :as hiccup]
+            [hiccup.util :as hu]))
 
-(def index
-  (page/root "Maria"
-    {:meta         {:viewport "width=device-width, initial-scale=1"}
-     :styles       [{:href "/trusted.css"}]
-     :body         [:div#maria-index]
-     :scripts/body [{:src "/js/compiled/trusted/shadow-trusted.js"}]}))
+(defn html [opts & contents]
+  (hiccup/html {:mode :html}
+               (hu/raw-string "<!DOCTYPE html>\n")
+               (into [:html opts] contents)))
 
-(def live
-  (page/root "Maria"
-    {:meta         {:viewport "width=device-width, initial-scale=1"}
-     :styles       [{:href "/tachyons.min.css"}
-                    {:href "/codemirror.css"}
-                    {:href "/maria.css"}]
-     :body         [:div#maria-env.h-100]
-     :scripts/body [{:src "/js/compiled/live/shadow-live.js"}]}))
+(defn index []
+  (html
+    (page/root "Maria"
+               {:meta {:viewport "width=device-width, initial-scale=1"}
+                :styles [{:href "/trusted.css"}]
+                :body [:div#maria-index]
+                :scripts/body [{:src (shadow/module-path :trusted :shadow-trusted)}]})))
+
+(defn live []
+  (html
+    (page/root "Maria"
+               {:meta {:viewport "width=device-width, initial-scale=1"}
+                :styles [{:href "/tachyons.min.css"}
+                         {:href "/codemirror.css"}
+                         {:href "/maria.css"}]
+                :body [:div#maria-env.h-100]
+                :scripts/body [{:src (shadow/module-path :live :shadow-live)}]})))
