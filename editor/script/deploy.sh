7dfebb353444d0cecf9c98e13ba7c4c82acfa3b0
Matthew Huebert <mhuebert@gmail.com>
2019-11-15 16:00:34 +0100

    fix deploy script

--- editor/script/deploy.sh
+++ editor/script/deploy.sh
@@ -2,10 +2,10 @@
 case "$CIRCLE_BRANCH" in
 "release")
   echo "Deploying to www.maria.cloud:";
-  npx firebase deploy -P "$FIREBASE_PROD" --only hosting;
+  npx firebase deploy -P maria-d04a7 --token "$FIREBASE_PROD" --only hosting;
   ;;
 "master")
   echo "Deploying to dev.maria.cloud";
-  npx firebase deploy -P "$FIREBASE_DEV" --only hosting;
+  npx firebase deploy -P maria-dev --token "$FIREBASE_DEV" --only hosting;
   ;;
 esac
