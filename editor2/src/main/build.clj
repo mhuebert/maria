6d0e720058bc6259857f6126f5bfeb4dd8b0cfc8
Matthew Huebert <mhuebert@gmail.com>
2022-09-27 16:38:00 +0200

    bg-color

--- editor2/src/main/build.clj
+++ editor2/src/main/build.clj
@@ -7,6 +7,6 @@
              {:meta {:viewport "width=device-width, initial-scale=1"}
               :scripts/head [{:src "https://cdn.tailwindcss.com?plugins=forms,typography"}]
               :styles [{:href "https://prosemirror.net/css/editor.css"}]
-              :props/html {:class "bg-gray-[#eee]"}
+              :props/html {:class "bg-[#eeeeee]"}
               :body [:div#maria-live]
               :scripts/body [{:src (shadow/module-path :editor :main)}]}))
