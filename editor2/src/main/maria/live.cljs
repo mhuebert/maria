a50511ab7e586ce8071ef1b34372badae419ab4e
Matthew Huebert <mhuebert@gmail.com>
2022-10-25 12:33:17 +0200

    document examples

--- editor2/src/main/maria/live.cljs
+++ editor2/src/main/maria/live.cljs
@@ -15,64 +15,39 @@
               (str/starts-with? title "http")
               (assoc :target "_blank")) title])
 
-(def js-interop-example "
+(def js-interop-examples "
+;; Using [applied-science/js-interop](https://www.github.com/applied-science/js-interop):
+
 (def obj #js{:x \"world\"})
 (j/get obj :x)
 (j/defn hello [^js {:keys [x]}] x)
 (hello obj)
 ")
 
-(def view-example
-  "(require '[yawn.view :as v])
+(def view-examples
+  "
+  ;; Using [yawn](https://github.com/mhuebert/yawn), a light React wrapper:
+
+  (require '[yawn.view :as v])
    (v/x [:div {:style {:color \"red\"}} \"a\"])
    (v/defview hello-world [name] [:div \"Hello, \" name])
    (hello-world \"creature of the void!\")")
 
-(def emoji-example "
+(def emoji-examples "
+;; Using emoji, thanks to [sci](https://www.github.com/babashka/sci) (this doesn't work in plain ClojureScript)
+
 (def 🔬 '{🦄 🌈
           🐄 🥩
           🤧 🦠})
 (🔬 '🦄)
 ")
 
-(def syntax-example "
-;; # Hello, world...
-
-(doc circle)
-
-(dir shapes.core)
-
-#_(do 10)
-
-{:a 10}
-
-(defn my-fn \"A docstring\" []
- 1 ;; number
- :abra ;; keyword
- cadabra ;; symbol
- #\"regex\" ;; regex
- \"string\"
- \\c ;; character
-  ...)
-
-;; Another Paragraph
-
-(prn 10)
-
-;; More text")
-
-(def para-example "
-;; One
-
-;; Two
-;; Three")
-
 (def error-examples "
-;; ## Other values
+;; Showing a \"raw\" javascript type
 
 js/Promise
 
-;; ## Errors
+;; Showing errors
 
 (10)
 
@@ -81,14 +56,19 @@ js/Promise
 
 (def promise-examples
   "
-;; ## Promises with top-level `await`
+;; Showing the status of a promise
+
+(require '[promesa.core :as p])
 
 (p/delay 3000 (square 20))
 
-;; You can await a `def` that contains a promise.
+;; When evaluating a namespace sequentially, `await` will pause
+;; until the awaited promise returns. You can also await a var
+;; which contains to a promise.
 
-(await
-  (def num-circles (p/delay 300 5)))
+(await (p/delay 1000 (square 20)))
+
+(await (def num-circles (p/delay 300 5)))
 
 (->> (range 1 (inc num-circles))
      (map #(circle (* % 10))))
@@ -109,7 +89,9 @@ js/Promise
 
 (def cell-examples "
 
-;; ## Cells
+;; Using the cells library.
+
+(require '[cells.api :refer :all])
 
 (defcell counter (interval 400 inc))
 
@@ -122,21 +104,23 @@ js/Promise
 (cell (timeout 1000 \"hello\"))
 
 
-;; ## Async state
+;; Cells show their 'async status' (ie. loading, error state)
+
 (for [i (range 10)]
   (cell (timeout (* i 300) (circle 10))))
 
 ")
 
 
-(def repl-examples ";; ## REPL utils
+(def repl-examples ";; Special REPL utilities
 (doc inc)
 (doc do)
 (doc when)
 (dir user)")
 
 (def ns-examples "
-;; Namespaces
+;; Working with namespaces. When evaluating a code block individually,
+;; Maria uses the nearest-evaluated namespace above it.
 
 (ns my.app)
 (doc circle)
@@ -147,7 +131,7 @@ js/Promise
 
 (def sicm-examples "
 
-;; ## SICM Utils
+;; Using SICM Utils
 (ns maria.sicm
   (:require [sicmutils.env :refer :all]))
 
@@ -185,6 +169,7 @@ js/Promise
 
 (def string-examples
   (str
+   ";; Rendering long strings - click to expand. (Should improve the affordance here.)\n"
    (pr-str "e great and striking peculiarity of this play is that its action lies wholly in the ideal world. It differs, therefore, from every other work of Shakespeare in the character of its mediation. Our poet, in most of his dramas, portrays the real world, and exhibits man as acting from clear conscious motives, and not from supernatural influences. But here he completely reverses his procedure; from beginning to end the chief instrumentalities of the poem are external; its conflicts and solutions are brought about by powers seemingly beyond human might and intelligence.\" J. D. Snider. Read on...\n \nSonnet 73\nAn analysis of Shakespeare's inspired sonnet, hailed as one of the best in the Western canon. Included is a paraphrase of the poem in contemporary English.\n \nWhy is King Leir Important?\nMr. Lawrence Barrett as King Lear.The story of King Lear and his three daughters is an old tale, well known in England for centuries before Shakespeare wrote the definitive play on the subject. The first English account of Lear can be found in the History of the Kings of Britain, written by Geoffrey Monmouth in 1135. However, it is clear that Shakespeare relied chiefly on King Leir, an anonymous play published twelve years before the first recorded performance of Shakespeare's King Lear. Read on to find out more about Leir and see side-by-side versions of Leir and Shakespeare's masterpiece.\n \nWhat Did Shakespeare Look Like?\nThe Stratford Bust, located on the wall of the chancel of Holy Trinity Church at Stratford-upon-Avon, is the oldest and, along with the Droeshout Portrait, most credible of all the known images of Shakespeare. But there are many representations of the Bard that have been handed down throughout the centuries, each with its own fascinating story to tell.\n \nShakespeare in Print: The Perils of Publishing in Elizabethan England\nOld printing press. From The Triumphs of the Printing Press. Walter Gerrold. London, Partridge & Co.During Shakespeare's lifetime Elizabethan playwrights cared little about seeing their work in print. Only the rare drama was actually intended to be read as well as performed. ")
    "\"a
 b
@@ -204,7 +189,8 @@ o\""
    ))
 
 (def link-examples
-  ";; Hello, [world](https://example.com) of [apples](https://www.apple.com) ![title](https://via.placeholder.com/350x150).
+  ";; Links and images in markdown become clickable and editable by moving the cursor into them.
+   ;; Hello, [world](https://example.com) of [apples](https://www.apple.com) ![title](https://via.placeholder.com/350x150).
   ")
 (def requires
   (str '(ns maria.examples
@@ -215,9 +201,9 @@ o\""
   [prose/editor {:source
                  (do
                    (str
-                    js-interop-example
-                    view-example
-                    emoji-example
+                    js-interop-examples
+                    view-examples
+                    emoji-examples
                     link-examples
                     repl-examples
                     ns-examples
