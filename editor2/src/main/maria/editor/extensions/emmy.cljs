196b4b30e9b029fbedebe9e2f93bf059a8bfa529
Matthew Huebert <mhuebert@gmail.com>
2023-07-21 21:20:17 -0600

    fix .MafsView font css

--- editor2/src/main/maria/editor/extensions/emmy.cljs
+++ editor2/src/main/maria/editor/extensions/emmy.cljs
@@ -1,5 +1,6 @@
 (ns maria.editor.extensions.emmy
-  (:require [emmy.viewer.sci]
+  (:require [clojure.string :as str]
+            [emmy.viewer.sci]
             [emmy.portal.css :refer [inject!] :rename {inject! inject-css!}]
             [emmy.viewer.css :refer [css-map]]
             [emmy.mafs]
@@ -16,9 +17,18 @@
             (:portal.viewer/reagent? m)
             (show/reagent-eval opts x)))))
 
+(defn inject-css-source! [source]
+  (let [style (.createElement js/document "style")]
+    (set! (.-type style) "text/css")
+    (.appendChild style (js/document.createTextNode source))
+    (js/document.head.appendChild style)))
+
 (defn install! []
   (show/add-global-viewers! (get-ctx) :before :vector [show-emmy])
   (ext.reagent/install!)
   (doseq [href (apply concat (vals css-map))]
-    (inject-css! href))
+    (if (= href "https://unpkg.com/mafs@0.17.0/font.css")
+      (inject-css-source!
+        ".MafsView { font-family: \"CMU Serif\", serif;}")
+      (inject-css! href)))
   (emmy.viewer.sci/install!))
